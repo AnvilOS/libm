78b7a5f6d7b39b61c1541de314dc013aa3e66c3b
Gerard Green <gerryg@anvilos.com>
2025-08-02 12:14:47 +1000

    Added 25000 digit pi

---
+++ tools/main.cpp
@@ -0,0 +1,16 @@
+
+//
+// Copyright (c) 2025 Gerard Green
+// All rights reserved
+//
+// Please see the file 'LICENSE' for further information
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ tools/pi_25000.hpp
@@ -0,0 +1,14 @@
+
+//
+// Copyright (c) 2025 Gerard Green
+// All rights reserved
+//
+// Please see the file 'LICENSE' for further information
+//
+
+#ifndef pi_25000_hpp
+#define pi_25000_hpp
+
+const char pi_25000[];
+
+#endif // pi_25000_hpp
