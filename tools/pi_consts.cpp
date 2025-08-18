357bb5ed05da9464c9788ee0cb3ab5a65da2236e
Gerard Green <gerryg@anvilos.com>
2025-08-18 18:49:16 +1000

    Calculate PI/2 to 256 bits

--- tools/pi_consts.cpp
+++ tools/pi_consts.cpp
@@ -13,7 +13,16 @@
 
 #include <string.h>
 
+void two_over_pi();
+void pi_over_two();
+
 void pi_consts()
+{
+    two_over_pi();
+    pi_over_two();
+}
+
+void two_over_pi()
 {
     // Check that there is a decimal point
     const char *p_dec = strchr(pi_25000, '.');
@@ -53,13 +62,52 @@ void pi_consts()
     xint_div(q, r, TWO, PI);
     xint_print_raw("2/PI", q);
     
-    for (int i=0; i<1152; ++i)
-    {
-        xint_mul_ulong(q, q, 10);
-    }
-    xint_rshift(q, q, 1152);
-    xint_print("2/PI", q);
+    xint_delete(PI);
+    xint_delete(TWO);
+    xint_delete(q);
+    xint_delete(r);
+}
 
+void pi_over_two()
+{
+    // Check that there is a decimal point
+    const char *p_dec = strchr(pi_25000, '.');
+    if (p_dec == NULL)
+    {
+        return;
+    }
+    char *end;
+    long val = strtol(pi_25000, &end, 10);
+    
+    xint_t PI;
+    xint_init(PI);
+    xint_assign_ulong(PI, val);
+    xint_from_dec_string(PI, end + 1);
+
+    xint_t TWO;
+    xint_init(TWO);
+    xint_assign_ulong(TWO, 2);
+    while (1)
+    {
+        if (xint_cmp(PI, TWO) < 0)
+        {
+            uint32_t r;
+            xint_div_1(TWO, &r, TWO, 10);
+            break;
+        }
+        xint_mul_ulong(TWO, TWO, 10);
+    }
+
+    xint_lshift(PI, PI, 252);
+
+    xint_t q;
+    xint_t r;
+    xint_init(q);
+    xint_init(r);
+
+    xint_div(q, r, PI, TWO);
+    xint_print_raw("PI/2", q);
+    
     xint_delete(PI);
     xint_delete(TWO);
     xint_delete(q);
